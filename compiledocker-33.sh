a9abc565ef691df6d4f95bb5df1eb249b821c245
jackybek <jackybek@gmail.com>
2023-12-30 20:42:48 +0800

    Update compiledocker-33.sh

--- compiledocker-33.sh
+++ compiledocker-33.sh
@@ -1,4 +1,4 @@
 sudo ./killdocker-33.sh
-sudo docker build -f dockerfile-33 -t opcuaserver33:v1.0 --tag 'opcuaserver' .
+sudo docker build -f dockerfile-33 -t opcuaserver33:v1.0 --tag 'opcuaserver33' .
 sudo docker run -it -d --name opcuaserver33 opcuaserver33 bash
 sudo docker exec -it opcuaserver33 /bin/bash
