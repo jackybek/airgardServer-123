04ac3e1ad1fbcb1fa6eee23420523b90b60f7f68
jackybek <jackybek@gmail.com>
2023-12-31 16:55:13 +0800

    Update SV_WebSockets.c

--- SV_WebSockets.c
+++ SV_WebSockets.c
@@ -5,10 +5,10 @@
 #include <open62541/server_config_default.h>
 #include <signal.h>
 #if defined(WIN32)
-	#define HAVE_STRUCT_TIMESPEC
-	#if defined(pid_t)
-	#undef pid_t
-	#endif
+ #define HAVE_STRUCT_TIMESPEC
+ #if defined(pid_t)
+  #undef pid_t
+ #endif
 #endif
 #include <pthread.h>
 
